56baf82068790ad266d7995095ebf1416cb81ace
Segoletca <sergey.noskov.2001@mail.ru>
2025-02-03 14:49:24 +0300

    Update annotation task

--- аннотации_задача.py
+++ аннотации_задача.py
@@ -34,8 +34,10 @@ def main(duel_res: list[tuple[int, str, bool]]) -> str:
             current_rep = add_rep(current_rep, rep, effect)
         if result == 'failure':
             current_rep = remove_rep(current_rep, rep, effect)
-    return f"""После {len(duel_res)} поединков,
-            репутация персонажа — {current_rep:.3f} очков."""
+    return (
+            f"После {len(duel_res)} поединков, "
+            f"репутация персонажа — {current_rep:.3f} очков."
+            )
 
 
 # Тестовый вызов функции main.
