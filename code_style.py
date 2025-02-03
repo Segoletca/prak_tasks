9b2551afd020e4659e421f8fc5128e559fd9a6bd
Segoletca <sergey.noskov.2001@mail.ru>
2025-02-03 14:45:19 +0300

    Add code style task

---
+++ code_style.py
@@ -0,0 +1,25 @@
+import math
+
+message = (
+    "Добро пожаловать в самую лучшую программу для вычисления "
+    "квадратного корня из заданного числа"
+)
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return math.sqrt(number)
+
+
+def calc(your_number):
+    """Сообщает о вычесленном корне."""
+    if your_number > 0:
+        result = calculate_square_root(your_number)
+        print(
+            ("Мы вычислили квадратный корень из введённого вами числа. "
+             f"Это будет: {result}")
+            )
+
+
+print(message)
+calc(25.5)
